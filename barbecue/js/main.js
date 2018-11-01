58721f7e7ccee62ad2ef4af488209075576f1d8b
anush <anush@example.ru>
2018-11-02 03:01:22 +0400

    optimization

---
+++ barbecue/js/main.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+
+    var swiper = new Swiper('.swiper-container', {
+        pagination: {
+            el: '.swiper-pagination',
+            type: 'fraction'
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev'
+        }
+    });
+
+});
+
+
+
